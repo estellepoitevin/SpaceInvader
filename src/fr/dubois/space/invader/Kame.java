aa190059f2fcc011a39992b125218178b95bfe35
tony.raux <tony.raux@103F11.etu.iut-lens.univ-artois.fr>
2012-11-26 12:20:20 +0100

    Création de la class Kame (Vaisseau)

---
+++ src/fr/dubois/space/invader/Kame.java
@@ -0,0 +1,21 @@
+// Création de la classe Kame (Vaisseau) 
+
+package fr.dubois.space.invader;
+
+import android.graphics.Bitmap;
+
+
+public class Kame extends Sprite{
+
+	public Kame(Bitmap bitmap, float x, float y) {
+		super(bitmap, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
